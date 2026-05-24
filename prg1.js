974b29288fc31fab0a771ede34d0c36bd6c36d9f
nikhil240458-max <nikhil240458@arkajainuniversity.ac.in>
2026-05-24 09:34:16 +0530

    table

--- prg1.js
+++ prg1.js
@@ -1,16 +1,10 @@
-const age=32
-if(age<18)
-{
-    console.log("not approved")
-}
-else if (age>=18 && age<60)
-{
-    console.log("approved")
-}
-else if(age>=60)
-{
-    console.log("senior approved")
-}
-else 
-{    console.log("you are dead")
+const age = 32;
+if (age < 18) {
+  console.log("not approved");
+} else if (age >= 18 && age < 60) {
+  console.log("approved");
+} else if (age >= 60) {
+  console.log("senior approved");
+} else {
+  console.log("you are dead");
 }
---
+++ prg2.js
@@ -0,0 +1,4 @@
+const table=2
+for(let i=1; i<=10; i++){
+    console.log(table+"*"+i+"="+table*i)
+}   
