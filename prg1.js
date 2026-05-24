c217f835803d2858560ae794441fb615e8299b89
nikhil240458-max <nikhil240458@arkajainuniversity.ac.in>
2026-05-24 09:17:04 +0530

    prg

---
+++ prg1.js
@@ -0,0 +1,16 @@
+const age=32
+if(age<18)
+{
+    console.log("not approved")
+}
+else if (age>=18 && age<60)
+{
+    console.log("approved")
+}
+else if(age>=60)
+{
+    console.log("senior approved")
+}
+else 
+{    console.log("you are dead")
+}
